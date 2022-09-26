b018deca90f70b7766a8a7bcf1aef78fc6134def
loic-dev <loic.charrie.12@gmail.com>
2022-09-26 18:56:52 +0200

    create header responsive

--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -1,29 +1,58 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./header.style.css";
+import "./header.style.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes, faChevronRight  } from '@fortawesome/free-solid-svg-icons'
+import {AiFillInstagram,AiFillCalendar,AiFillPhone,AiFillFacebook} from "react-icons/ai"
 
 
 export const HeaderComponent = () => {
+    const [headerMobile, setHeaderMobile] = useState(false);
+
+
+   
     return (
-        <nav>
-            <div className="logo">
-                <img></img>
-            </div>
-            <div className="link-container">
-                <ul>
-                    <li>
-                        <Link to="/events">evenements</Link>
-                    </li> 
-                    <li>
-                        <Link to="/contact">contact</Link>
-                    </li> 
-                    <li className="social">
-                        <a href="https://www.facebook.com">facebook</a>
-                        <a href="https://www.instagram.com">instagram</a>
-                    </li>
-                </ul>
-                
-            </div>
-            
+        <nav className="header">
+            <div className="header-container">
+                <div className="container">
+                    <div className="logo">
+                        logo
+                    </div>
+                    <div className="mobileMenuIconContainer">
+                        <span onClick={() => setHeaderMobile(!headerMobile)}><FontAwesomeIcon className="mobileMenuIcon"  icon={headerMobile ? faTimes : faBars}/></span>
+                    </div>
+                </div>
+                <div className={`link-container ${headerMobile ? "open" : "close"}`}>
+                    <ul>
+                        <li>
+                            <Link className="link" to="/events">
+                                <AiFillCalendar className="linkIcon" fontSize={"1.4rem"}/>
+                                <span>événements</span>
+                                <FontAwesomeIcon className="chevronLinkIcon"  icon={faChevronRight}/>
+                            </Link>
+                        </li> 
+                        <li>
+                            <Link className="link" to="/contact">
+                                <AiFillPhone className="linkIcon" fontSize={"1.4rem"}/>
+                                <span>contact</span>
+                                <FontAwesomeIcon className="chevronLinkIcon"  icon={faChevronRight}/>
+                            </Link>
+                        </li> 
+                        <li className="social">
+                            <a target="_blank" href="https://www.instagram.com">
+                                <AiFillInstagram fontSize={"1.4rem"}/>
+                                <span>Suivez nous sur Instagram</span>
+                            </a>
+                        </li>
+                        <li className="social">
+                            <a target="_blank" className="showDesktop" href="https://www.facebook.com">
+                                <AiFillFacebook fontSize={"1.4rem"}/>
+                                <span>Suivez nous sur facebook</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>       
         </nav>
     )
 }
